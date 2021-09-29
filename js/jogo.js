584f4f60f84a8902a80b04843b8e15dec6dd098a
Éverton <evertonvictor2019br@gmail.com>
2021-09-28 21:07:12 -0300

    Recuperar o ajuste na Tela
    Foi criado uma função que permite recuperar a altura e largura, conforme as mudanças as dimensões da tela

---
+++ js/jogo.js
@@ -0,0 +1,9 @@
+var altura = 0
+var largura = 0
+
+function ajustarTamanhoPalcoJogo() {
+  altura = window.screen.availHeight
+  largura = window.screen.availWidth
+
+  console.log(altura, largura)
+}
